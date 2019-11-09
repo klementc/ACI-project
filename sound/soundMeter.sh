a9f2e9f4cb0f28d371e1c1be507be1a7f4b8982e
Clément Courageux <clementcourageuxsudan@gmail.com>
2019-11-09 18:44:20 +0100

    sound script from romain

---
+++ sound/soundMeter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo `mkdir /home/pi/soundMeter && cd /home/pi/soundMeter && touch dBValues.txt`
+
+destdir=/home/pi/soundMeter/dBValues.txt
+
+while true
+do
+
+        record=`arecord -D plughw:1,0 -d $1 record.wav`
+
+        sndfileInfo=`sndfile-info record.wav | grep -oP "(-\d{2}\.\d{2})"`
+
+
+        if [ -f "$destdir" ]
+        then
+                echo "$sndfileInfo" >> "$destdir"
+                echo "done"
+        fi
+
+        echo `rm record.wav`
+        sleep $2
+done

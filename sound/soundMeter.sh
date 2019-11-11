2250d2dd9e77ed6d16b7bdb41a2ada9b11b86381
Clément Courageux <clementcourageuxsudan@gmail.com>
2019-11-11 12:41:32 +0100

    better sound

--- sound/soundMeter.sh
+++ sound/soundMeter.sh
@@ -7,10 +7,9 @@ destdir=/home/pi/soundMeter/dBValues.txt
 while true
 do
 
-        record=`arecord -D plughw:1,0 -d $1 record.wav`
-
+#        record=`arecord -D plughw:1,0 -d $1 record.wav`
+	record=`arecord -D hw:1,0  -f S16_LE -r 48000 -d 5 record.wav`
         sndfileInfo=`sndfile-info record.wav | grep -oP "(-\d{1,2}\.\d{1,2})"`
-
 
         if [ -f "$destdir" ]
         then

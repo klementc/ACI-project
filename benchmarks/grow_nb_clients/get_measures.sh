1e9cd6012ea8e363572a47fd5f9eb9f439f7b8b4
hahaha <hahaha>
2019-11-17 20:13:43 +0100

    test

--- benchmarks/grow_nb_clients/get_measures.sh
+++ benchmarks/grow_nb_clients/get_measures.sh
@@ -3,13 +3,20 @@
 declare -a arr=("1" "10" "100" "200" "500" "750" "1000")
 
 echo "get metrics for get and set on random keys according to number of requests"
-
+rm dataSETrpc.dat
+rm dataSET.dat
 for i in "${arr[@]}"
 do
    filename=100000req_${i}_parallel_clients.log
    echo "values for $i: $filename"
    redis-benchmark -r 100000 -n 100000 -c $i -t set > $filename
+   a=`cat ${filename} | pcregrep -M " requests per second" | awk  -v v="$i" -F"requests per second" '{print $1}' |sort -n `
+   echo "$i $a" >> dataSETrpc.dat
+   b=`cat ${filename} | pcregrep -M "====== SET ======\n.*requests completed in" | grep "requests completed in" | awk -F"requests completed in|seconds" '{print $2}' |sort -n`
+   echo "$i $b" >> dataSET.dat
 done
 
 
-cat *.log | pcregrep -M "====== SET ======\n.*requests completed in" | grep "requests completed in" | awk -F"requests completed in|seconds" '{print $1 $2}' |sort -n > dataSET.dat 
+#cat *.log | pcregrep -M "====== SET ======\n.*requests completed in" | grep "requests completed in" | awk -F"requests completed in|seconds" '{print $1 $2}' |sort -n > dataSET.dat 
+
+#cat *.log | pcregrep -M " requests per second" | awk  -v v="$i" -F"requests per second" '{print $a $1}' |sort -n > dataSETrps.dat 
--- benchmarks/grow_nb_clients/plot.py
+++ benchmarks/grow_nb_clients/plot.py
@@ -10,11 +10,28 @@ with open('dataSET.dat','r') as file:
 		print(row)
 		x.append(float(row[0]))
 		y.append(float(row[1]))
+x2 = []
+y2 = []
+with open('dataSETrpc.dat','r') as file:
+        for row in file:
+                row = row.strip().split(" ")
+                print(row)
+                x.append(float(row[0]))
+                y.append(float(row[1]))
 
-plt.xlabel('Number of requests')
-plt.ylabel('Duration to complete all requests (s)')
-plt.plot(x,y, marker="o")
-plt.xscale("log")
+fig, ax1 = plt.subplots()
+
+col='tab:red'
+ax1.set_xlabel('Number of requests')
+ax1.set_ylabel('Duration to complete all requests (s)')
+ax1.plot(x,y, marker="o", color=col)
+ax1.set_xscale("log")
+
+ax2 = ax1.twinx()
+col='tab:blue'
+ax2.set_ylabel('Duration to complete all requests (s)')
+ax2.bar(x,y, color=col)
+
 plt.legend()
 plt.savefig("grownbcl.pdf")
 
